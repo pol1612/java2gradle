9d65369171fcd86f2bc3daa6d68fa5b869a8901a
Pol Sañé Jové <pol.sane@uvic.cat>
2022-03-21 20:06:07 +0100

    We have created an Square object and made a code that calculates its area and side

--- cat/uvic/teknos/m06/java2gradle/App.java
+++ cat/uvic/teknos/m06/java2gradle/App.java
@@ -1,7 +1,15 @@
 package cat.uvic.teknos.m06.java2gradle;
 
 public class App {
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-	}
+    public static int squareArea(int side){
+        return side*side;
+    }
+    public static void main(String[] args){
+        var square1= new Square();
+        square1.side=10;
+        square1.computeArea();
+        System.out.println("Square 1  side: " +square1.side+" Square 1 area: "+square1.area());
+        
+    }
+    
 }
---
+++ cat/uvic/teknos/m06/java2gradle/Square.java
@@ -0,0 +1,9 @@
+package cat.uvic.teknos.m06.java2gradle;
+
+public class Square {
+    public int side;
+    public int area;
+    public void computeArea(){
+        area=side*side;
+    }
+}
