c4fa5bd24d967736819d4bfb04e89954f0062220
Pol Sañé Jové <pol.sane@uvic.cat>
2022-03-22 20:39:30 +0100

    Imported external library and used it

--- cat/uvic/teknos/m06/java2gradle/App.java
+++ cat/uvic/teknos/m06/java2gradle/App.java
@@ -1,14 +1,14 @@
 package cat.uvic.teknos.m06.java2gradle;
-
+import org.apache.commons.lang3.math.NumberUtils;
 public class App {
-    public static int squareArea(int side){
-        return side*side;
-    }
     public static void main(String[] args){
-        var square1= new Square();
-        square1.side=10;
-        square1.computeArea();
-        System.out.println("Square 1  side: " +square1.side+" Square 1 area: "+square1.area());
+        //var square1= new Square();
+        //square1.side=10;
+        //square1.computeArea();
+        var integers = new int[] {23,45,1,0};
+        //var numberUtils = new NumberUtils
+        System.out.println("Min value {23,45,1,0}"+NumberUtils.min(integers));
+        //System.out.println("Square 1  side: " +square1.side+" Square 1 area: "+square1.area());
         
     }
     
